d5e7631793735012a25f2c0aa30591573bccf92a
Chris LeBlanc <crleblanc@gmail.com>
2016-11-17 15:17:31 +1300

    more progress, have a line that updates it's length based on difference in XYZ

--- generate_kml2.py
+++ generate_kml2.py
@@ -3,41 +3,41 @@
 
 import simplekml
 
+
+def kmlCoords(coords):
+    return ','.join([str(x) for x in coords])
+
+def absoluteChange(orig, delta):
+    return ','.join([str(x+y) for x, y in zip(orig, delta)])
+
+
 kml = simplekml.Kml(name='GPS Sensor displacement for Kaikoura Earthquake 14 Nov 2016', open=1)
 
 # pnt = kml.newpoint(coords=[(170.144,-43.605)])
 # pnt.style.iconstyle.scale = 1.0
 
+# example station coodinates, lon, lat, elev.
+station_coords = [170.12, -43.65, 1000]
+
 # Adding a line, as a poor man's arrow.  I guess we could make an arrow from several segments as a func...
 # TODO: start with a line of length zero (no displacement), use a func to modify it's length based on n/e.  Make it a vector.  Could do 3D
 ln = kml.newlinestring(name='A LineString')
-ln.coords = [(170.12, -43.65, 1000), (170.14, -43.60, 1000)]
-ln.style.linestyle.width = 1
+ln.coords = [station_coords, station_coords]
+ln.style.linestyle.width = 10
 ln.style.linestyle.color = simplekml.Color.red
 ln.altitudemode = simplekml.AltitudeMode.relativetoground
 
-tour = kml.newgxtour(name="Play me!")
+tour = kml.newgxtour(name="Play displacement animation")
 playlist = tour.newgxplaylist()
 
-# soundcue = playlist.newgxsoundcue()
-# soundcue.href = "http://code.google.com/p/simplekml/source/browse/samples/drum_roll_1.wav"
-# soundcue.gxdelayedstart = 2
+for i, delta in enumerate([[0, 0.3, 0], [0, 0.1, 0], [0, 0.5, 0], [0, -0.2, 0]]):
 
-animatedupdate = playlist.newgxanimatedupdate(gxduration=1.0)
-animatedupdate.update.change = '<LineStyle targetId="{0}"><width>500.0</width></LineStyle>'.format(ln.style.linestyle.id)
+    animatedupdate = playlist.newgxanimatedupdate(gxduration=0.2)
+    updateStr = '<LineString targetId="{0}"><coordinates>{1} {2}</coordinates></LineString>'
+    animatedupdate.update.change = updateStr.format(ln.id, kmlCoords(station_coords), absoluteChange(station_coords, delta))
 
-wait = playlist.newgxwait(gxduration=1.0)
-animatedupdate2 = playlist.newgxanimatedupdate(gxduration=1.0)
-animatedupdate2.update.change = '<LineStyle targetId="{0}"><width>1.0</width></LineStyle>'.format(ln.style.linestyle.id)
+    wait = playlist.newgxwait(gxduration=0.2)
 
-# flyto = playlist.newgxflyto(gxduration=0.1)
-# flyto.camera.longitude = 170.157
-# flyto.camera.latitude = -43.671
-# flyto.camera.altitude = 9700
-# flyto.camera.heading = -6.333
-# flyto.camera.tilt = 33.5
-# flyto.camera.roll = 0
+wait = playlist.newgxwait(gxduration=0.1)
 
-wait = playlist.newgxwait(gxduration=2.0)
-
-kml.save("tut_tours.kml")
+kml.save("test.kml")
